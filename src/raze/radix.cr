d543b720499dea1a39e65f6239d91e86dead2467
Sam Eaton <sameaton11@gmail.com>
2017-09-16 20:10:26 -0700

    add comments for clarifying

--- src/raze/radix.cr
+++ src/raze/radix.cr
@@ -11,14 +11,18 @@ class Raze::Radix(T)
   end
 
   def find(path)
+    # if the route already has a cached result, use it
     if cached_result = @cached_results[path]?
       return cached_result
     end
 
     result = @tree.find(path)
+
+    # if the route has an associated stack and there are no dynamic params, cache it
     if result.found? && result.params.empty?
       @cached_results[path] = result
     end
+
     result
   end
 end
